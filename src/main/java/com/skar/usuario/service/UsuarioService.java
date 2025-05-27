5d45c28d1e6d2342773b29b7eb54df057e5f9aea
marceloriv <ma.riveracontreras@duocuc.cl>
2025-05-26 20:09:36 -0400

    Agregar métodos para obtener y actualizar usuarios en la interfaz UsuarioService

--- src/main/java/com/skar/usuario/service/UsuarioService.java
+++ src/main/java/com/skar/usuario/service/UsuarioService.java
@@ -7,11 +7,42 @@ import com.skar.usuario.dto.ApiRespuestaDto;
 import com.skar.usuario.dto.RegistracionUsuarioDto;
 import com.skar.usuario.exception.ErrorLogicaServicioUsuarioException;
 import com.skar.usuario.exception.UsuarioYaExisteException;
+import com.skar.usuario.model.Usuario;
 
 @Service
 public interface UsuarioService {
 
     ResponseEntity<ApiRespuestaDto> registrarUsuario(RegistracionUsuarioDto nuevoUsuarioDto)
             throws UsuarioYaExisteException, ErrorLogicaServicioUsuarioException;
+
+    ResponseEntity<Object> obtenerUsuarioPorEmail(String email)
+            throws ErrorLogicaServicioUsuarioException;
+
+    ResponseEntity<Object> obtenerUsuarioPorTelefono(String telefono)
+            throws ErrorLogicaServicioUsuarioException;
+
+    ResponseEntity<Object> obtenerUsuarioPorId(Long id)
+            throws ErrorLogicaServicioUsuarioException;
+
+    ResponseEntity<Object> obtenerUsuarioPorNombre(String nombre)
+            throws ErrorLogicaServicioUsuarioException;
+
+    ResponseEntity<Object> obtenerPorEstado(Boolean estado)
+            throws ErrorLogicaServicioUsuarioException;
+
+    ResponseEntity<Object> obtenerTodosLosUsuarios()
+            throws ErrorLogicaServicioUsuarioException;
+
+    ResponseEntity<Object> actualizarUsuario(Long id, Usuario usuarioActualizado)
+            throws ErrorLogicaServicioUsuarioException;
+
+    ResponseEntity<Object> eliminarUsuario(Long id)
+            throws ErrorLogicaServicioUsuarioException;
+
+    ResponseEntity<Object> cambiarEstadoUsuario(Long id, Boolean estado)
+            throws ErrorLogicaServicioUsuarioException;
+
+    ResponseEntity<Object> actualizarUsuarioPorId(Long id, RegistracionUsuarioDto usuarioDto)
+            throws ErrorLogicaServicioUsuarioException, UsuarioYaExisteException;
 
 }
