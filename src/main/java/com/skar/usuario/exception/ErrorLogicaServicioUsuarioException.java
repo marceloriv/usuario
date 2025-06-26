2c341e25c35c4e5e5b89592d322a99f95db2ba27
marceloriv <ma.riveracontreras@duocuc.com>
2025-06-26 15:05:01 -0400

    Corregir mensaje de excepción en ErrorLogicaServicioUsuarioException para incluir el mensaje específico del error

--- src/main/java/com/skar/usuario/exception/ErrorLogicaServicioUsuarioException.java
+++ src/main/java/com/skar/usuario/exception/ErrorLogicaServicioUsuarioException.java
@@ -3,7 +3,7 @@ package com.skar.usuario.exception;
 public class ErrorLogicaServicioUsuarioException extends Exception {
 
     public ErrorLogicaServicioUsuarioException(String message) {
-        super("Error en la lógica del servicio de usuario: ");
+        super("Error en la lógica del servicio de usuario: " + message);
     }
 
 }
