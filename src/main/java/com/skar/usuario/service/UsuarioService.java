e2610415c15b7238377940703aba243a1c9f1f3a
marceloriv <ma.riveracontreras@duocuc.cl>
2025-06-26 13:25:37 -0400

    formateo de la clase userservice

--- src/main/java/com/skar/usuario/service/UsuarioService.java
+++ src/main/java/com/skar/usuario/service/UsuarioService.java
@@ -12,37 +12,37 @@ import com.skar.usuario.model.Usuario;
 @Service
 public interface UsuarioService {
 
-    ResponseEntity<ApiRespuestaDto> registrarUsuario(RegistracionUsuarioDto nuevoUsuarioDto)
-            throws UsuarioYaExisteException, ErrorLogicaServicioUsuarioException;
+        ResponseEntity<ApiRespuestaDto> registrarUsuario(RegistracionUsuarioDto nuevoUsuarioDto)
+                        throws UsuarioYaExisteException, ErrorLogicaServicioUsuarioException;
 
-    ResponseEntity<Object> obtenerUsuarioPorEmail(String email)
-            throws ErrorLogicaServicioUsuarioException;
+        ResponseEntity<Object> obtenerUsuarioPorEmail(String email)
+                        throws ErrorLogicaServicioUsuarioException;
 
-    ResponseEntity<Object> obtenerUsuarioPorTelefono(String telefono)
-            throws ErrorLogicaServicioUsuarioException;
+        ResponseEntity<Object> obtenerUsuarioPorTelefono(String telefono)
+                        throws ErrorLogicaServicioUsuarioException;
 
-    ResponseEntity<Object> obtenerUsuarioPorId(Long id)
-            throws ErrorLogicaServicioUsuarioException;
+        ResponseEntity<Object> obtenerUsuarioPorId(Long id)
+                        throws ErrorLogicaServicioUsuarioException;
 
-    ResponseEntity<Object> obtenerUsuarioPorNombre(String nombre)
-            throws ErrorLogicaServicioUsuarioException;
+        ResponseEntity<Object> obtenerUsuarioPorNombre(String nombre)
+                        throws ErrorLogicaServicioUsuarioException;
 
-    ResponseEntity<Object> obtenerPorEstado(Boolean estado)
-            throws ErrorLogicaServicioUsuarioException;
+        ResponseEntity<Object> obtenerPorEstado(Boolean estado)
+                        throws ErrorLogicaServicioUsuarioException;
 
-    ResponseEntity<Object> obtenerTodosLosUsuarios()
-            throws ErrorLogicaServicioUsuarioException;
+        ResponseEntity<Object> obtenerTodosLosUsuarios()
+                        throws ErrorLogicaServicioUsuarioException;
 
-    ResponseEntity<Object> actualizarUsuario(Long id, Usuario usuarioActualizado)
-            throws ErrorLogicaServicioUsuarioException;
+        ResponseEntity<Object> actualizarUsuario(Long id, Usuario usuarioActualizado)
+                        throws ErrorLogicaServicioUsuarioException;
 
-    ResponseEntity<Object> eliminarUsuario(Long id)
-            throws ErrorLogicaServicioUsuarioException;
+        ResponseEntity<Object> eliminarUsuario(Long id)
+                        throws ErrorLogicaServicioUsuarioException;
 
-    ResponseEntity<Object> cambiarEstadoUsuario(Long id, Boolean estado)
-            throws ErrorLogicaServicioUsuarioException;
+        ResponseEntity<Object> cambiarEstadoUsuario(Long id, Boolean estado)
+                        throws ErrorLogicaServicioUsuarioException;
 
-    ResponseEntity<Object> actualizarUsuarioPorId(Long id, RegistracionUsuarioDto usuarioDto)
-            throws ErrorLogicaServicioUsuarioException, UsuarioYaExisteException;
+        ResponseEntity<Object> actualizarUsuarioPorId(Long id, RegistracionUsuarioDto usuarioDto)
+                        throws ErrorLogicaServicioUsuarioException, UsuarioYaExisteException;
 
 }
