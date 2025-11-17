d5fa01acc0368b01641ff7ecb5dd77d2267b0bc1
marceloriv <ma.riveracontreras@duocuc.cl>
2025-11-17 19:51:36 -0300

    Agregar configuración de Docker y soporte para AWS Aurora MySQL

--- src/test/java/com/skar/usuario/UsuarioApplicationTests.java
+++ src/test/java/com/skar/usuario/UsuarioApplicationTests.java
@@ -1,8 +1,15 @@
 package com.skar.usuario;
 
+import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("test")
 class UsuarioApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
 
 }
